7156c5b29dd1a8bbbcec8adefa8571e00e68367b
wjrsetbruce <57582311+wjrsetbruce@users.noreply.github.com>
2019-11-16 19:25:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,21 +12,16 @@ function Login(){
         window.location="template/cliente.html"; 
     }
     if(username === "" || username === "null" || password === "" || password === "null"){
-        alertify.error("Error, los campos se encuentran vacios");
+        alertify.error("Error, los campos se encuentran vacíos");
     }
     if(!validarEmail.test(username)){
-        alertify.error("Error, correo elèctronico no valido");
+        alertify.error("Error, correo eléctronico no válido");
         document.getElementById('correo').value = "";
     }
 
     if(username != "admin@cryptolab.net" && username != "william@gmail.com" && validarEmail.test(username)){
         alertify.error("Usuario no registrado");
         document.getElementById('correo').value = "";
-        document.getElementById('pass').value = "";
-    }
-
-    if(username === 'william@gmail.com' && password != "1"){
-        alertify.error("Contraseña Incorrecta");
         document.getElementById('pass').value = "";
     }
 }
@@ -44,27 +39,22 @@ function Registro(){
     validarCedula = /[A-Z]|\s|[a-z]/;
     validarEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     if(validarNombre.test(nombre)){
-        alertify.error('El nombre no puede contener numeros');
+        alertify.error('El nombre no puede contener números');
         document.getElementById('nombre').value = "";
         flag = false;
     }
     if(validarCedula.test(cedula)){
-        alertify.error('La cedula no puede contener letras');
-        document.getElementById('cedula').value = "";
-        flag = false;
-    }
-    if(cedula === '18736475'){
-        alertify.error('Error, la cedula ya se encuentra registrada');
+        alertify.error('La cédula no puede contener letras');
         document.getElementById('cedula').value = "";
         flag = false;
     }
     if(validarCedula.test(tlf)){
-        alertify.error('El telefono no puede contener letras');
-        document.getElementById('tlf').value = "";
+        alertify.error('El teléfono no puede contener letras');
+        document.getElementById('telefono').value = "";
         flag = false;
     }
     if(!validarEmail.test(correo)){
-        alertify.error("Error, correo elèctronico no valido");
+        alertify.error("Error, correo eléctronico no válido");
         document.getElementById('correo').value = "";
         flag = false;
     }
@@ -72,7 +62,7 @@ function Registro(){
         || tlf === "null" || tlf === "" || direccion ==="" || direccion ==="null" ||
          correo === "null" || correo ===""|| pass === "null" || pass === ""){
             // Muestro el error
-        alertify.error("Error, algunos campos se encuentran vacio");
+        alertify.error("Error, algunos campos se encuentran vacíos");
         flag = false;
     }
 
@@ -90,36 +80,25 @@ function modificarDatos(){
     var password = document.getElementById('password').value;
     flag = true;
     validarEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    validarTlf = /[A-Z]|\s|[a-z]/;
     if(!validarEmail.test(correo)){
-        alertify.error("Error, correo elèctronico no valido");
+        alertify.error("Error, correo eléctronico no válido");
         document.getElementById('email').value = "";
         flag = false;
     }
-
-    if(validarTlf.test(tlf)){
-        alertify.error('El telefono no puede contener letras');
-        document.getElementById('telefono').value = "";
-        flag = false;
-    }
-
     if(tlf === "" || tlf === "null" || direccion === "" || direccion === "null" ||
          correo === "null" || correo ===""|| password === "null" || password === ""){
             // Muestro el error
-         alertify.error("Error, algunos campos se encuentran vacio");
+         alertify.error("Error, algunos campos se encuentran vacíos");
          flag = false;
     }
 
     if(flag){
-        alertify.alert('Gestión Inmobiliaria', 'Modificaciòn Exitosa!', function(){
+        alertify.alert('Gestión Inmobiliaria', 'Modificación Exitosa!', function(){
             alertify.success('Ok'); window.location="cliente.html";})
 
     }
-}
 
-function Locales(){
-    alertify.alert('Gestión Inmobiliaria', 'Informaciòn del Loca y la Oficina', function(){
-        alertify.success('Ok'); window.location="consultar_servicio.html";})
 
+    
 }
 
