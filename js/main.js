1bf83be505c9df239da323d0379e96fea7a608e5
DrNykterstein <46200971+DrNykterstein@users.noreply.github.com>
2019-11-19 19:21:30 -0400

    Opinion lista

--- js/main.js
+++ js/main.js
@@ -219,3 +219,7 @@ function VerDetalles(){
         alertify.success('Ok'); window.location="consultar_cita_admin.html";})
 }
 
+function ReporteOpinion(){
+    alertify.alert('Grupo Gestión Inmobiliaria - Reporte', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.', function(){
+        alertify.success('Ok'); window.location="consultar_opinion.html";})
+}
